ae76041ce19495a8c84eecf6c3dee9fbbdfb8a49
ekaeoq <ekaeoq@gmail.com>
2020-02-21 16:02:30 +0100

    Initial commit

---
+++ mainPage/LoadingSiteRedirect.js
@@ -0,0 +1,18 @@
+/*sigma naslov redirect
+-koristi se loading gif(slow.gif)--loadingPage folder
+-svaki redirect sluzi za redirect do loading page-a(pa dalje ide)
+*/
+
+function Redirect(){
+    location.href="../loadingPage/indexLoading.html"
+}
+
+/*Hanin char click-redirect do njezinog loading page-a*/
+function hanaPage (){
+	location.href="../loadingPageHana/indexLoadingHana.html"
+}
+
+/*ekaeo char click-redirect do njegovo loading page-a*/
+function ekaeoPage() {
+	location.href="../loadingPageEkaeo/indexLoadingEkaeo.html"
+}
