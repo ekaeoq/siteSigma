83ba4b00c612fe98f4ac1469e05e6c7a3fdd5360
ekaeoq <ekaeoq@gmail.com>
2020-03-05 11:36:24 +0100

    comment + redirects

---
+++ commentSection/indexCommentSection.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post your comment here, buddy:)</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <button class="btn btn-primary">Register</button>
+                <button class="btn btn-success">Success</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+
+
+<script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+</body>
+</html>
--- mainPage/LoadingSiteRedirect.js
+++ mainPage/LoadingSiteRedirect.js
@@ -3,16 +3,22 @@
 -svaki redirect sluzi za redirect do loading page-a(pa dalje ide)
 */
 
+/*loadingPage folder where the gif file is located*/
 function Redirect(){
-    location.href="../loadingPage/indexLoading.html"
+    location.href="../loadingPage/indexLoading.html";
 }
 
 /*Hanin char click-redirect do njezinog loading page-a*/
 function hanaPage (){
-	location.href="../loadingPageHana/indexLoadingHana.html"
+	location.href="../loadingPageHana/indexLoadingHana.html";
 }
 
 /*ekaeo char click-redirect do njegovo loading page-a*/
 function ekaeoPage() {
-	location.href="../loadingPageEkaeo/indexLoadingEkaeo.html"
+	location.href="../loadingPageEkaeo/indexLoadingEkaeo.html";
+}
+/*Redirect do ListLoadingFolder-a gdje su smjesteni redirecti do
+novih html stranica s temom navedenom na <a> tagu u listi*/
+function RedirectLi_1() {
+	loacation.href="../ListLoadingFolder/indexLoadingLi_1.html";
 }
